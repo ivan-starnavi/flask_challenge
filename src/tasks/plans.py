ff92d3b75282b21303695f52445790863f46d9c0
Ivan Podkidishev <ivan.podkidishev@starnavi.io>
2020-06-18 11:27:14 +0400

    started solving task 2

--- src/tasks/plans.py
+++ src/tasks/plans.py
@@ -1,17 +1,19 @@
 """Plan related tasks"""
 from celery.utils.log import get_task_logger
-from flask import current_app
 
 from src.celery_app import celery
+from src.models.subscriptions import Subscription
+from src.models.cycles import BillingCycle
 
 log = get_task_logger(__name__)
 
 
 @celery.task()
-def query_subscription_plans():
+def query_subscription_plans(subscription_id, billing_cycle_id):
     """Add google style doc string here
 
     (https://www.sphinx-doc.org/en/1.7/ext/example_google.html)
 
     """
-    pass
+    subscription = Subscription.query.get(subscription_id)
+    billing_cycle = BillingCycle.query.get(billing_cycle_id)
